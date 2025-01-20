51a5bf9b9bb47f2f2418be6bd38a85073a5e4f74
Sathish <rhbsk1994@gmail.com>
2025-01-20 08:22:58 +0530

    Added build for vite config - to reload assets in browser

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Netflix-Clone-React/'
+  base: '/Netflix-Clone-React/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  }
+  
 })
